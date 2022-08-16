9f6cb2987c1777b0e44cfa9e4dfd5cc100e80bd2
klynndido <klynndido1@gmail.com>
2022-08-15 21:43:22 -0600

    books with spence

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,14 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import sqlite3 from 'sqlite3';
-import * as sqlite from 'sqlite';
-import express from 'express';
-import bodyParser from 'body-parser';
+import path from "path";
+import { fileURLToPath } from "url";
+import sqlite3 from "sqlite3";
+import * as sqlite from "sqlite";
+import express from "express";
+import bodyParser from "body-parser";
 
 // Connect to the database
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dbFilename = 'books.db';
+const dbFilename = "books.db";
 const dbPath = path.resolve(__dirname, `../db/${dbFilename}`);
 const db = await sqlite.open({ filename: dbPath, driver: sqlite3.Database });
 
@@ -16,16 +16,19 @@ const db = await sqlite.open({ filename: dbPath, driver: sqlite3.Database });
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/books', async (req, res) => {
-  const books = await db.all('SELECT * FROM book;');
+app.get("/books", async (req, res) => {
+  const books = await db.all("SELECT * FROM book;");
   res.json(books);
+  console.log("hello");
 });
 
-app.post('/books', async (req, res) => {
+app.post("/books", async (req, res) => {
+  res.json({ Message: req.body.title });
+});
+app.get("/create-test-book", async (req, res) => {
+  await db.run("INSERT INTO book (title) VALUES ('My Test Book')");
   res.json({});
 });
-
-
 // Start the server
 const port = 4000 || process.env.PORT;
 app.listen(port, () => {
